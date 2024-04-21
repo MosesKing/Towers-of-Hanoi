c7cd079e360a644792c0cc750fdaee22ece1d5d3
Moesaeah King <moefromthemo@gmail.com>
2024-04-21 00:13:50 -0700

    Removing Double Hanoi.Hanoi
    Changing all references to just Hanoi

--- tower-challenge-operator/api/v1alpha1/groupversion_info.go
+++ tower-challenge-operator/api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the hanoi v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=hanoi.hanoi.com
+// +groupName=hanoi.com
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "hanoi.hanoi.com", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "hanoi.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
